01f63821bdd1c30c3b034cedbdedf866316fd91b
TheoWongkar <theoterra19@gmail.com>
2025-01-09 14:09:02 +0800

    Memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('/berita', [PostController::class, 'index'])->name('posts.index');
+Route::get('/berita/{slug}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware(['auth', 'auth.session'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
