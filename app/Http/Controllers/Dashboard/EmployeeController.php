13d1554ecf3e5ad0420414ccfdc7dc5227db6d71
TheoWongkar <theoterra19@gmail.com>
2025-01-13 21:31:04 +0800

    Membuat fitur employee create dan store

--- app/Http/Controllers/Dashboard/EmployeeController.php
+++ app/Http/Controllers/Dashboard/EmployeeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeeController extends Controller
 {
@@ -42,11 +43,63 @@ class EmployeeController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $title = "Tambah Karyawan";
+
+        return view('dashboard.employee.create', compact('title'));
+    }
+
+    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, string $nip)
+    public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nip' => 'required|string|max:255|unique:employees',
+            'name' => 'required|string|max:255',
+            'gender' => 'required|in:Pria,Wanita',
+            'position' => 'required|string|max:255',
+            'date_of_birth' => 'required|date',
+            'address' => 'required|string|max:255',
+            'phone' => 'required|string|max:13',
+            'picture' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'role' => 'required|in:user,admin,operator',
+            'username' => 'required|string|max:255|unique:users,name',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($request->hasFile('picture')) {
+            $imagePath = $request->file('picture')->store('employees', 'public');
+            $validated['picture'] = $imagePath;
+        } else {
+            $imagePath = null;
+        }
+
+        // Menyimpan data employee
+        $employee = Employee::create([
+            'nip' => $validated['nip'],
+            'name' => $validated['name'],
+            'gender' => $validated['gender'],
+            'position' => $validated['position'],
+            'date_of_birth' => $validated['date_of_birth'],
+            'address' => $validated['address'],
+            'phone' => $validated['phone'],
+            'picture' => $validated['picture'],
+        ]);
+
+        // Menyimpan data user dengan nama yang diambil dari username
+        $employee->user()->create([
+            'role' => $validated['role'],
+            'name' => $validated['username'],  // Menggunakan username untuk kolom name
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return redirect()->route('dashboard.employees.index')->with('success', 'Karyawan berhasil ditambahkan.');
     }
 
     /**
@@ -56,9 +109,25 @@ class EmployeeController extends Controller
     {
         $title = "Karyawan" . $nip;
 
-        $employee = Employee::withTrashed()->where('nip', $nip)->firstOrFail();
+        $employee = Employee::with(['user', 'posts', 'reports'])->withTrashed()->where('nip', $nip)->firstOrFail();
 
         return view('dashboard.employee.show', compact('title', 'employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $nip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $nip)
+    {
+        //
     }
 
     /**
