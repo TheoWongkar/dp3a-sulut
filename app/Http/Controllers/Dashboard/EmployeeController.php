df4115ad487b02fb926044ac9deef0b82b88bc6d
TheoWongkar <theoterra19@gmail.com>
2025-01-13 23:40:46 +0800

    Membuat employee edit. menerapkan soft delete

--- app/Http/Controllers/Dashboard/EmployeeController.php
+++ app/Http/Controllers/Dashboard/EmployeeController.php
@@ -6,6 +6,7 @@ use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class EmployeeController extends Controller
 {
@@ -119,7 +120,11 @@ class EmployeeController extends Controller
      */
     public function edit(string $nip)
     {
-        //
+        $title = "Ubah Karyawan" . $nip;
+
+        $employee = Employee::with('user')->withTrashed()->where('nip', $nip)->firstOrFail();
+
+        return view('dashboard.employee.edit', compact('title', 'employee'));
     }
 
     /**
@@ -127,8 +132,69 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, string $nip)
     {
-        //
+        $employee = Employee::withTrashed()->where('nip', $nip)->firstOrFail();
+
+        $validated = $request->validate([
+            'nip' => 'required|string|max:255|unique:employees,nip,' . $nip . ',nip',
+            'name' => 'required|string|max:255',
+            'gender' => 'required|in:Pria,Wanita',
+            'position' => 'required|string|max:255',
+            'date_of_birth' => 'required|date',
+            'address' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:13',
+            'picture' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'role' => 'required|in:user,admin,operator',
+            'username' => 'required|string|max:255|unique:users,name,' . $employee->user->name . ',name',
+            'email' => 'required|email|max:255|unique:users,email,' . $employee->user->email . ',email',
+            'password' => 'nullable|string|min:8|confirmed',
+            'status' => 'required|in:active,inactive',
+        ]);
+
+
+        if ($request->hasFile('picture')) {
+            if ($employee->picture) {
+                Storage::disk('public')->delete($employee->picture);
+            }
+
+            $imagePath = $request->file('picture')->store('employees', 'public');
+            $validated['picture'] = $imagePath;
+        }
+
+        $employee->update([
+            'nip' => $validated['nip'],
+            'name' => $validated['name'],
+            'gender' => $validated['gender'],
+            'position' => $validated['position'],
+            'date_of_birth' => $validated['date_of_birth'],
+            'address' => $validated['address'],
+            'phone' => $validated['phone'] ?? $employee->phone,
+            'picture' => $validated['picture'] ?? $employee->picture,
+        ]);
+
+        $employee->user->update([
+            'role' => $validated['role'],
+            'name' => $validated['username'],
+            'email' => $validated['email'],
+            'password' => $validated['password'] ? Hash::make($validated['password']) : $employee->user->password,
+        ]);
+
+        // Perbarui status
+        if ($request->status == 'active') {
+            // Restore jika soft deleted
+            if ($employee->trashed()) {
+                $employee->restore();
+            }
+        } elseif ($request->status == 'inactive') {
+            // Soft delete jika belum terhapus
+            if (!$employee->trashed()) {
+                $employee->delete();
+            }
+        }
+
+        return redirect()->route('dashboard.employees.index')->with('success', 'Karyawan berhasil diubah.');
     }
+
+
 
     /**
      * Remove the specified resource from storage.
